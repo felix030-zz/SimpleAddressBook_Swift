4ccfa459b93b08f3383ec96448ad95e54be2d5b1
FKlemke <s0545427@htw-berlin.de>
2016-01-08 12:59:45 +0100

    aktuell

--- Ue5_6-Klemke-Fiebig/MasterViewController.swift
+++ Ue5_6-Klemke-Fiebig/MasterViewController.swift
@@ -12,7 +12,7 @@ class MasterViewController: UITableViewController {
   
   var detailViewController: DetailViewController? = nil
   var objects = AddressBook()
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     //adding test data
@@ -44,24 +44,37 @@ class MasterViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  //    func insertNewObject(sender: AnyObject) {
-  //      objects.insert(NSDate(), atIndex: 0)
-  //      let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-  //      self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-  //    }
+  func insertNewObject(sender: AnyObject) {
+    //        objects.insert(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+  }
   
   // MARK: - Segues
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
-      if let indexPath = self.tableView.indexPathForSelectedRow {
-        let object = objects
-        let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-        controller.detailItem = object
-        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-        controller.navigationItem.leftItemsSupplementBackButton = true
+      if let cont = segue.destinationViewController as? DetailViewController {
+        if let indexPath = self.tableView.indexPathForSelectedRow {
+          //          cont.indexPath = indexPath
+          //          cont.book = book
+          
+          let adrsCardSection = objects.getCellFromSectionIndexPath(indexPath.section)
+          let adrsCard = adrsCardSection[indexPath.row]
+        }
       }
     }
   }
+  
+  
+  //  if let indexPath = self.tableView.indexPathForSelectedRow {
+  //    let object = objects
+  //
+  //
+  //    let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+  //    controller.detailItem = object
+  //    controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+  //    controller.navigationItem.leftItemsSupplementBackButton = true
+  //  }
   
   // MARK: - Sections
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
